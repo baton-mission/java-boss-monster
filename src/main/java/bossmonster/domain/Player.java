0c3ce16d3c70120276daeeac209befd08b263dbb
yuseonjun <seonjun0906@gmail.com>
2023-09-22 17:09:55 +0900

    feat: 플레이어의 이름은 5자 이하만 가능하다.

--- src/main/java/bossmonster/domain/Player.java
+++ src/main/java/bossmonster/domain/Player.java
@@ -5,6 +5,13 @@ public class Player {
 	private final String name;
 
 	public Player(String name) {
+		validatePlayerLength(name);
 		this.name = name;
+	}
+
+	private void validatePlayerLength(String name) {
+		if (name.length() > 5) {
+			throw new IllegalArgumentException("[ERROR] 플레이어의 이름은 5자 이하만 가능합니다.");
+		}
 	}
 }
--- src/test/java/bossmonster/domain/PlayerTest.java
+++ src/test/java/bossmonster/domain/PlayerTest.java
@@ -6,13 +6,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-@DisplayName("BossMonster")
-public class BossMonsterTest {
+@DisplayName("Player")
+class PlayerTest {
 
 	@ParameterizedTest
-	@ValueSource(ints = {99, 301})
-	void 생성자는_100이상_300이하가_아니면_IllegalArgumentException가_발생한다(int hp) {
-		assertThatThrownBy(() -> new BossMonster(hp))
+	@ValueSource(strings = {"안녕하세요오", "안녕하십니까"})
+	void 생성자는_5자_이하가_아닌경우_IllegalArgumentException을_던진다(String name) {
+		assertThatThrownBy(() -> new Player(name))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
+
