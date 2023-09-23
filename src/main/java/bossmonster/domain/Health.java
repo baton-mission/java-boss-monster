bafb813c3fe8ddec69bdb495dbbd84154b523043
yuseonjun <seonjun0906@gmail.com>
2023-09-23 16:42:18 +0900

    refactor: 매직 넘버 상수화

--- src/main/java/bossmonster/domain/Health.java
+++ src/main/java/bossmonster/domain/Health.java
@@ -1,9 +1,20 @@
 package bossmonster.domain;
 
+import static bossmonster.domain.AttackType.*;
+
 public class Health {
+
+	private static final int HP_ZERO = 0;
+	private static final int MIN_BOSS_HP = 100;
+	private static final int MAX_BOSS_HP = 300;
+	private static final int SUM_HP_MP = 200;
 
 	private int hp;
 	private int mp;
+
+	public Health(int hp) {
+		this.hp = hp;
+	}
 
 	public Health(int hp, int mp) {
 		this.hp = hp;
@@ -11,15 +22,23 @@ public class Health {
 	}
 
 	public boolean validateBossHpRange() {
-		return hp < 100 || hp > 300;
+		return hp < MIN_BOSS_HP || hp > MAX_BOSS_HP;
 	}
 
 	public boolean validatePlayerHpAndMp() {
-		return (hp + mp) != 200;
+		return (hp + mp) != SUM_HP_MP;
 	}
 
 	public boolean isHpZero() {
-		return hp <= 0;
+		if (hp <= HP_ZERO) {
+			setHpZero();
+			return true;
+		}
+		return false;
+	}
+
+	private void setHpZero() {
+		hp = HP_ZERO;
 	}
 
 	public void spendHp(int damage) {
@@ -31,12 +50,16 @@ public class Health {
 	}
 
 	public void calculateMp(AttackType attackType) {
-		if (attackType.equals(AttackType.PHYSICAL)) {
+		if (attackType.equals(PHYSICAL)) {
 			mp += attackType.getEffect();
 		}
-		if (attackType.equals(AttackType.MAGIC)) {
+		if (attackType.equals(MAGIC)) {
 			mp -= attackType.getEffect();
 		}
+	}
+
+	public boolean checkPlayerMp(AttackType type) {
+		return mp < type.getEffect();
 	}
 
 	public int getHp() {
--- src/main/java/bossmonster/domain/RandomDamageGenerator.java
+++ src/main/java/bossmonster/domain/RandomDamageGenerator.java
@@ -4,9 +4,11 @@ import java.util.Random;
 
 public class RandomDamageGenerator implements DamageGenerator {
 
+	private static final int BOSS_DAMAGE_MAX_RANGE = 21;
+
 	@Override
 	public int generate() {
 		Random random = new Random();
-		return random.nextInt(21);
+		return random.nextInt(BOSS_DAMAGE_MAX_RANGE);
 	}
 }
