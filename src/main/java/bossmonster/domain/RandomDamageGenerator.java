f779738286bf946c7c3165e32f209f14288eaaff
yuseonjun <seonjun0906@gmail.com>
2023-09-24 18:02:18 +0900

    refactor: 호출 될때마다 새로운 객체를 생성하지 않고, 한번 생성한 객체를 재사용 하게 리팩터링

--- src/main/java/bossmonster/domain/RandomDamageGenerator.java
+++ src/main/java/bossmonster/domain/RandomDamageGenerator.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class RandomDamageGenerator implements DamageGenerator {
 
 	private static final int BOSS_DAMAGE_MAX_RANGE = 21;
+	private final Random random = new Random();
 
 	@Override
 	public int generate() {
-		Random random = new Random();
 		return random.nextInt(BOSS_DAMAGE_MAX_RANGE);
 	}
 }
