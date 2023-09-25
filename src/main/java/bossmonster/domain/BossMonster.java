2219d96abf8c9803cdca8b4baedb6a1398becabe
SehyeonKang <gedfgrdt@naver.com>
2023-09-25 19:48:59 +0900

    feat(BossMonster): add BossMonster class

---
+++ src/main/java/bossmonster/domain/BossMonster.java
@@ -0,0 +1,12 @@
+package bossmonster.domain;
+
+public class BossMonster {
+
+    int hp;
+    int turnCount;
+
+    public BossMonster(int hp) {
+        this.hp = hp;
+        turnCount = 1;
+    }
+}
