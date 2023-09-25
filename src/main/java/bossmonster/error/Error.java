eeb015204c64cae763e9461240503c2e1c0c57fb
kung036 <sinariari3@gmail.com>
2023-09-25 12:23:12 +0900

    feat(Error): Error 프린트

---
+++ src/main/java/bossmonster/error/Error.java
@@ -0,0 +1,8 @@
+package bossmonster.error;
+
+public class Error {
+    public static void printError(String error) {
+        System.out.println("[ERROR] " + error);
+        System.out.println("다시 입력하세요.");
+    }
+}
