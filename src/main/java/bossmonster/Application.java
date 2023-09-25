ebb24879fe04a17c8354e3e81175b1e53e2d429a
SehyeonKang <gedfgrdt@naver.com>
2023-09-25 23:28:17 +0900

    feat(Application): update Application

--- src/main/java/bossmonster/Application.java
+++ src/main/java/bossmonster/Application.java
@@ -1,7 +1,10 @@
 package bossmonster;
 
+import bossmonster.controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GameController gameController = new GameController();
+        gameController.play();
     }
 }
