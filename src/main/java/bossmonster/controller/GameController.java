3ad9c7c259ce6be35cd9a8587ccb55129719c4d3
SehyeonKang <gedfgrdt@naver.com>
2023-09-25 23:05:37 +0900

    feat(GameController): add progressInitialSetting method

--- src/main/java/bossmonster/controller/GameController.java
+++ src/main/java/bossmonster/controller/GameController.java
@@ -5,6 +5,9 @@ import bossmonster.domain.Player;
 import bossmonster.service.Battle;
 import bossmonster.service.InitialSetting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameController {
 
     Player player;
@@ -18,5 +21,22 @@ public class GameController {
     }
 
     public void play() {
+        progressInitialSetting();
+    }
+
+    public void progressInitialSetting() {
+        /**
+         * 임시 데이터 삽입
+         */
+        String playerName = "testName";
+        List<Integer> playerStatus = new ArrayList<>();
+        playerStatus.add(150);
+        playerStatus.add(50);
+        int bossMonsterHp = 150;
+
+        // TODO: 뷰에서 데이터 받아와서 삽입
+
+        initialSetting.setPlayerStatus(playerName, playerStatus);
+        initialSetting.setBossMonsterStatus(bossMonsterHp);
     }
 }
