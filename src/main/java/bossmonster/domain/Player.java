2bf381e2fd19cb65c36a297d35323551fee0daf5
SehyeonKang <gedfgrdt@naver.com>
2023-09-25 22:40:32 +0900

    feat(Player): update Player class
    maxMp 멤버 변수 추가

--- src/main/java/bossmonster/domain/Player.java
+++ src/main/java/bossmonster/domain/Player.java
@@ -8,11 +8,13 @@ public class Player {
     String name;
     int hp;
     int mp;
+    int maxMp;
 
     public Player(String name, List<Integer> status) {
         this.name = name;
         this.hp = status.get(0);
         this.mp = status.get(1);
+        this.maxMp = status.get(1);
     }
 
     public String getName() {
@@ -28,7 +30,7 @@ public class Player {
     }
 
     public void gainMp() {
-        if (mp < 100) {
+        if (mp < maxMp) {
             mp += 10;
         }
     }
