159013b9f788c0c672613b7141b5fa0927472eb0
soochan <tncksdl05@gmail.com>
2023-09-26 23:30:20 +0900

    Feat(PlayerMp): 플레이어 Mp 객체 추가

--- src/main/java/bossmonster/domain/ExceptionMessage.java
+++ src/main/java/bossmonster/domain/ExceptionMessage.java
@@ -5,6 +5,8 @@ public final class ExceptionMessage {
     public static final String BOSS_HP_RANGE_EXCEPTION_MESSAGE = "보스 몬스터의 HP는 100이상 300이하가 되어야 합니다.";
     public static final String INITIAL_HP_MP_SUM_EXCEPTION_MESSAGE = "초기 HP와 MP 합은 200이어야 합니다.";
     public static final String PLAYER_HP_EXCEPTION_MESSAGE = "초기 플레이어 HP는 0보다 커야 합니다.";
+    public static final String PLAYER_MP_EXCEPTION_MESSAGE = "초기 플레이어 MP는 0보다 커야 합니다.";
+
 
     private ExceptionMessage() {
     }
---
+++ src/main/java/bossmonster/domain/PlayerMp.java
@@ -0,0 +1,33 @@
+package bossmonster.domain;
+
+import static bossmonster.domain.ExceptionMessage.PLAYER_MP_EXCEPTION_MESSAGE;
+
+public class PlayerMp {
+
+    private static final int MIN_MP = 0;
+
+    private final int playerMp;
+
+    private PlayerMp(int playerMp) {
+        validate(playerMp);
+        this.playerMp = playerMp;
+    }
+
+    private void validate(int playerMp) {
+        validateUnderMinMp(playerMp);
+    }
+
+    private void validateUnderMinMp(int playerMp) {
+        if (isUnderMinMp(playerMp)) {
+            throw new IllegalArgumentException(PLAYER_MP_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean isUnderMinMp(int playerMp) {
+        return playerMp <= MIN_MP;
+    }
+
+    public static PlayerMp from(int playerMp) {
+        return new PlayerMp(playerMp);
+    }
+}
