833d98999ace6d7b2fb485aa632b2588032fe3af
SehyeonKang <gedfgrdt@naver.com>
2023-09-26 09:24:34 +0900

    feat(GameView): add printEndGameView method

--- src/main/java/bossmonster/view/GameView.java
+++ src/main/java/bossmonster/view/GameView.java
@@ -7,6 +7,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class GameView {
+
+    final int BOSS_NORMAL = 0;
+    final int BOSS_DAMAGED = 1;
+    final int BOSS_WIN = 2;
+
     Scanner scanner = new Scanner(System.in);
 
     public int printBossHpSettingView() {
@@ -28,8 +33,8 @@ public class GameView {
         return parseToIntegerList(inputString);
     }
 
-    public int printPlayerPhaseView(BattleInfoDto battleInfoDto) {
-        printBattleInfoView(battleInfoDto);
+    public int printPlayerPhaseView(BattleInfoDto battleInfoDto, int turnCount) {
+        printBattleInfoView(battleInfoDto, turnCount);
         System.out.println("어떤 공격을 하시겠습니까?");
         System.out.println("1. 물리 공격");
         System.out.println("2. 마법 공격");
@@ -50,16 +55,54 @@ public class GameView {
         System.out.println("보스가 공격했습니다. (입힌 대미지: " + bossDamage + ")");
     }
 
-    private void printBattleInfoView(BattleInfoDto battleInfoDto) {
+    public void printEndGameByVictoryView(BattleInfoDto battleInfoDto, int turnCount) {
+        System.out.println(battleInfoDto.getPlayerName() + " 님이 " + turnCount + "번의 전투 끝에 보스 몬스터를 잡았습니다.");
+    }
+
+    public void printEndGameByDefeatView(BattleInfoDto battleInfoDto, int turnCount) {
+        printBattleInfoView(battleInfoDto, turnCount);
+        System.out.println(battleInfoDto.getPlayerName() + "의 HP가 0이 되었습니다");
+        System.out.println("보스 레이드에 실패했습니다.");
+    }
+
+    private void printBattleInfoView(BattleInfoDto battleInfoDto, int turnCount) {
         System.out.println("============================");
         System.out.println("BOSS HP [" + battleInfoDto.getBossHp() + "/" + battleInfoDto.getBossMaxHp() + "]");
         System.out.println("____________________________");
-        System.out.println("  ^-^\n / 0 0 \\\n(   \"   )\n \\  -  /\n  - ^ -");
+
+        if (turnCount == 1) {
+            printBossMonsterView(BOSS_NORMAL);
+        }
+
+        if (turnCount > 1 && battleInfoDto.getPlayerHp() > 0) {
+            printBossMonsterView(BOSS_DAMAGED);
+        }
+
+        if (turnCount > 1 && battleInfoDto.getPlayerHp() == 0) {
+            printBossMonsterView(BOSS_WIN);
+        }
+
         System.out.println("____________________________");
         System.out.println(battleInfoDto.getPlayerName()
                 + " HP [" + battleInfoDto.getPlayerHp() + "/" + battleInfoDto.getPlayerMaxHp()
                 + "] MP [" + battleInfoDto.getPlayerMp() + "/" + battleInfoDto.getPlayerMaxMp() +"]");
         System.out.println("============================\n");
+    }
+
+    private void printBossMonsterView(int bossCondition) {
+        if (bossCondition == BOSS_NORMAL) {
+            System.out.println("  ^-^\n / 0 0 \\\n(   \"   )\n \\  -  /\n  - ^ -");
+        }
+
+        if (bossCondition == BOSS_DAMAGED) {
+            System.out.println("  ^-^\n / x x \\\n(   \"   )\n \\  -  /\n  - ^ -");
+
+        }
+
+        if (bossCondition == BOSS_WIN) {
+            System.out.println("  ^-^\n / ^ ^ \\\n(   \"   )\n \\  -  /\n  - ^ -");
+
+        }
     }
 
     private List<Integer> parseToIntegerList(String inputString) {
