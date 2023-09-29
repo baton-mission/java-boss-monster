ece23df24d2f21d1ae9ab4fb703d038d086c6839
edgar6bf <edgar6bf@gmail.com>
2023-09-30 04:32:05 +0900

    feat: PlayerName 초기화 기능 구현
    - 입력으로 받은 값을 검증하고, 플레이어의 이름을 초기화
    - 빈 문자열 혹은 5 초과 길이의 문자열이면 예외 발생

---
+++ src/main/java/bossmonster/domain/player/constant/PlayerOption.java
@@ -0,0 +1,5 @@
+package bossmonster.domain.player.constant;
+
+public class PlayerOption {
+    public static final int PLAYER_NAME_MAXIMUM_LENGTH_LIMIT = 5;
+}
---
+++ src/test/java/bossmonster/domain/player/PlayerNameTest.java
@@ -0,0 +1,52 @@
+package bossmonster.domain.player;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("플레이어 이름 기능 테스트")
+class PlayerNameTest {
+    @DisplayName("[성공 테스트] PlayerName 인스턴스 생성 시 유효한 플레이어 이름을 전달하면, PlayerName 인스턴스가 생성된다.")
+    @Test
+    void 유효한_플레이어_이름_객체_생성_테스트() throws Exception {
+        // Given
+        String validPlayerName = "edgar";
+
+        // When
+        PlayerName playerName = new PlayerName(validPlayerName);
+
+        // Then
+        assertThat(playerName.getPlayerName()).isEqualTo(validPlayerName);
+    }
+
+    @DisplayName("[예외 테스트] PlayerName 인스턴스 생성 시 빈 문자열을 전달하면, 예외가 발생한다.")
+    @Test
+    void 비어있는_플레이어_이름_객체_생성_테스트() throws Exception {
+        // Given
+        String emptyPlayerName = "";
+
+        // When
+        Throwable throwable = catchThrowable(() -> new PlayerName(emptyPlayerName));
+
+        // Then
+        assertThat(throwable)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유효하지 않은 플레이어 이름입니다.");
+    }
+
+    @DisplayName("[예외 테스트] PlayerName 인스턴스 생성 시 허용 길이 보다 긴 문자열을 전달하면, 예외가 발생한다.")
+    @Test
+    void 허용_길이_보다_긴_플레이어_이름_객체_생성_테스트() throws Exception {
+        // Given
+        String tooLongPlayerName = "abcedefhijkejefffeefefef";
+
+        // When
+        Throwable throwable = catchThrowable(() -> new PlayerName(tooLongPlayerName));
+
+        // Then
+        assertThat(throwable)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유효하지 않은 플레이어 이름입니다.");
+    }
+}
