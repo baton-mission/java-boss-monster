7b5d59080860a95566a5a2846c09235d7c324e7e
Hanjaemo <hjm2530@gmail.com>
2023-09-30 11:28:39 +0900

    feat(InputView): 플레이어 이름 입력 기능 구현

--- src/main/java/bossmonster/view/InputView.java
+++ src/main/java/bossmonster/view/InputView.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class InputView {
 
     public static int readBossMonsterHp(Scanner scanner) {
-        return scanner.nextInt();
+        return toInt(scanner.nextLine());
+    }
+
+    private static int toInt(String string) {
+        return Integer.parseInt(string);
+    }
+
+    public static String readPlayerName(Scanner scanner) {
+        return scanner.nextLine();
     }
 }
