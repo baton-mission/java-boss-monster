d35e852b609f8408c21649d01b823181be678a1f
Hanjaemo <hjm2530@gmail.com>
2023-09-30 12:01:04 +0900

    feat(OutputView): 보스 몬스터의 공격 결과 출력

--- src/main/java/bossmonster/view/OutputView.java
+++ src/main/java/bossmonster/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public static void printStartMessage() {
         System.out.println("보스 레이드를 시작합니다!");
     }
+
+    public static void printBossMonsterAttackResult(int bossMonsterDamage) {
+        System.out.printf("보스가 공격 했습니다. (입힌 데미지: %d)\n", bossMonsterDamage);
+    }
 }
