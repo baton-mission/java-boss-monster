209cff3677fdfe7d5896b488334573ec7280f236
soochan <tncksdl05@gmail.com>
2023-09-30 21:22:15 +0900

    Test(BossTest): 테스트 코드 추가

---
+++ src/test/java/bossmonster/domain/BossTest.java
@@ -0,0 +1,32 @@
+package bossmonster.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+
+class BossTest {
+
+    @Test
+    void attackTo는_플레이어에게_데미지를_주고_데미지를_리턴한다() {
+        Player mockPlayer = mock(Player.class);
+        int bossHp = 100;
+        Boss boss = Boss.from(bossHp, (min, max) -> 20);
+
+        int bossDamage = boss.attackTo(mockPlayer);
+
+        verify(mockPlayer).attackedBy(20);
+        assertThat(bossDamage).isEqualTo(20);
+    }
+
+    @Test
+    void zeroDamage는_0을_리턴한다() {
+        int bossHp = 100;
+        Boss boss = Boss.from(bossHp, (min, max) -> 20);
+
+        int zeroDamage = boss.zeroDamage();
+
+        assertThat(zeroDamage).isEqualTo(0);
+    }
+}
