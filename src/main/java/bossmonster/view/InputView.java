3c2c73e80a1b454eb5e363b087392a6e1226ddb4
Hanjaemo <hjm2530@gmail.com>
2023-09-30 11:40:13 +0900

    feat(InputView): 플레이어 공격 번호 입력 기능 구현

--- src/main/java/bossmonster/view/InputView.java
+++ src/main/java/bossmonster/view/InputView.java
@@ -12,10 +12,6 @@ public class InputView {
         return toInt(scanner.nextLine());
     }
 
-    private static int toInt(String string) {
-        return Integer.parseInt(string);
-    }
-
     public static String readPlayerName(Scanner scanner) {
         return scanner.nextLine();
     }
@@ -25,5 +21,13 @@ public class InputView {
                         .split(DELIMITER))
                 .map(InputView::toInt)
                 .toList();
+    }
+
+    public static int readPlayerAttackNumber(Scanner scanner) {
+        return toInt(scanner.nextLine());
+    }
+
+    private static int toInt(String string) {
+        return Integer.parseInt(string);
     }
 }
