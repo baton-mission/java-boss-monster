7fe30118e78ce7b8da7ee77a1a94952937299ec2
Hanjaemo <hjm2530@gmail.com>
2023-09-30 11:39:01 +0900

    feat(InputView): 플레이어의 초기 HP, MP 입력 기능 구현

--- src/main/java/bossmonster/view/InputView.java
+++ src/main/java/bossmonster/view/InputView.java
@@ -1,8 +1,12 @@
 package bossmonster.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+
+    private static final String DELIMITER = ",";
 
     public static int readBossMonsterHp(Scanner scanner) {
         return toInt(scanner.nextLine());
@@ -14,5 +18,12 @@ public class InputView {
 
     public static String readPlayerName(Scanner scanner) {
         return scanner.nextLine();
+    }
+
+    public static List<Integer> readPlayerInitialHpAndMp(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine()
+                        .split(DELIMITER))
+                .map(InputView::toInt)
+                .toList();
     }
 }
