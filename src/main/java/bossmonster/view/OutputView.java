67dd915c5590d74bcc0249627e98e9242e150f10
Hanjaemo <hjm2530@gmail.com>
2023-09-30 14:15:11 +0900

    feat(OutputView): 플레이어의 정보 출력

--- src/main/java/bossmonster/view/OutputView.java
+++ src/main/java/bossmonster/view/OutputView.java
@@ -1,13 +1,23 @@
 package bossmonster.view;
 
+import java.util.Map;
+
 public class OutputView {
 
     public static void printStartMessage() {
         System.out.println("보스 레이드를 시작합니다!");
     }
 
-    public static void printBossMonsterInfo(int hp, int initialHp) {
-        System.out.printf("BOSS HP [%d/%d]\n", hp, initialHp);
+    public static void printBossMonsterInfo(Map<String, Integer> info) {
+        System.out.printf("BOSS HP [%d/%d]\n", info.get("hp"), info.get("initialHp"));
+    }
+
+    public static void printPlayerInfo(String name, Map<String, Integer> info) {
+        Integer hp = info.get("hp");
+        Integer mp = info.get("mp");
+        Integer initialHp = info.get("initialHp");
+        Integer initialMp = info.get("initialMp");
+        System.out.printf("%s HP [%d/%d] MP [%d/%d]", name, hp, initialHp, mp, initialMp);
     }
 
     public static void printBossMonsterAppearance(String appearance) {
