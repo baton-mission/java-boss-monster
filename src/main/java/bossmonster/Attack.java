def92c15b48bfc31c9cb0a3d53dde4c60d03db8b
Hanjaemo <hjm2530@gmail.com>
2023-09-30 12:09:00 +0900

    feat(OutputView): 플레이어의 공격 결과 출력

--- src/main/java/bossmonster/Attack.java
+++ src/main/java/bossmonster/Attack.java
@@ -2,15 +2,17 @@ package bossmonster;
 
 public enum Attack {
 
-    PHYSICAL(1, 10, -10),
-    MAGIC(2, 20, 30);
+    PHYSICAL(1, "물리 공격", 10, -10),
+    MAGIC(2, "마법 공격", 20, 30);
 
     private int number;
+    private String name;
     private int damage;
     private int mpConsumption;
 
-    Attack(int number, int damage, int mpConsumption) {
+    Attack(int number, String name, int damage, int mpConsumption) {
         this.number = number;
+        this.name = name;
         this.damage = damage;
         this.mpConsumption = mpConsumption;
     }
--- src/main/java/bossmonster/view/OutputView.java
+++ src/main/java/bossmonster/view/OutputView.java
@@ -6,6 +6,10 @@ public class OutputView {
         System.out.println("보스 레이드를 시작합니다!");
     }
 
+    public static void printPlayerAttackResult(String name, int damage) {
+        System.out.printf("%s을 했습니다. (입힌 데미지: %d)\n", name, damage);
+    }
+
     public static void printBossMonsterAttackResult(int bossMonsterDamage) {
         System.out.printf("보스가 공격 했습니다. (입힌 데미지: %d)\n", bossMonsterDamage);
     }
