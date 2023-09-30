be56a83ff4c4dc525b20a93f029be1a1d58a05a8
Hanjaemo <hjm2530@gmail.com>
2023-09-30 11:22:04 +0900

    feat(InputView): 보스 몬스터의 HP 입력 기능 구현

---
+++ src/main/java/bossmonster/view/InputView.java
@@ -0,0 +1,10 @@
+package bossmonster.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public static int readBossMonsterHp() {
+        return new Scanner(System.in).nextInt();
+    }
+}
