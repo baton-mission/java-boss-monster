700774ff2186dbbcdb2e889a1a14421c1a079fe2
Hanjaemo <hjm2530@gmail.com>
2023-09-30 11:43:05 +0900

    feat(InputView): 각 입력마다 안내 문구 출력

--- src/main/java/bossmonster/view/InputView.java
+++ src/main/java/bossmonster/view/InputView.java
@@ -9,14 +9,17 @@ public class InputView {
     private static final String DELIMITER = ",";
 
     public static int readBossMonsterHp(Scanner scanner) {
+        System.out.println("보스 몬스터의 HP를 입력해주세요.");
         return toInt(scanner.nextLine());
     }
 
     public static String readPlayerName(Scanner scanner) {
+        System.out.println("플레이어의 이름을 입력해주세요.");
         return scanner.nextLine();
     }
 
     public static List<Integer> readPlayerInitialHpAndMp(Scanner scanner) {
+        System.out.println("플레이어의 HP와 MP를 입력해주세요.(,로 구분)");
         return Arrays.stream(scanner.nextLine()
                         .split(DELIMITER))
                 .map(InputView::toInt)
@@ -24,6 +27,9 @@ public class InputView {
     }
 
     public static int readPlayerAttackNumber(Scanner scanner) {
+        System.out.println("어떤 공격을 하시겠습니까?\n" +
+                           "1. 물리 공격\n" +
+                           "2. 마법 공격");
         return toInt(scanner.nextLine());
     }
 
