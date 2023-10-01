93fa956d81c217b3e6bda66b68cad1e3aed96617
twkwon0417 <91003152+twkwon0417@users.noreply.github.com>
2023-10-01 23:44:56 +0900

    <feat> : boss의 체력 인풋을 검증하는 코드 구현

--- src/main/java/bossmonster/domain/BossMonster.java
+++ src/main/java/bossmonster/domain/BossMonster.java
@@ -23,7 +23,11 @@ public class BossMonster {
         return this.maxHp;
     }
 
-    public int attack() {
-        return rand.nextInt(19) + 1;
+    public static int validateBossHp(int hp) {
+        if (hp > MAX_HP_SIZE || hp < MIN_HP_SIZE) {
+            throw new IllegalArgumentException(
+                    "[Error] Boss monster's health should be in between 100 to 300");
+        }
+        return hp;
     }
 }
