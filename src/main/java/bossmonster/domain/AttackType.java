cf71fbb249ea9ca6316f686290325133ccb4df79
soochan <tncksdl05@gmail.com>
2023-10-02 15:33:14 +0900

    Refactor(AttackType): 캐시 전략을 사용하도록 수정

--- src/main/java/bossmonster/domain/AttackType.java
+++ src/main/java/bossmonster/domain/AttackType.java
@@ -4,12 +4,21 @@ import static bossmonster.domain.ExceptionMessage.ATTACK_TYPE_EXCEPTION_MESSAGE;
 import static bossmonster.domain.MpStatus.RECOVERY;
 import static bossmonster.domain.MpStatus.REDUCE;
 
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Map;
 
 public enum AttackType {
 
     PHYSICAL(1, "물리 공격", 10, RECOVERY),
     MAGICAL(2, "마법 공격", 20, REDUCE);
+
+    private static final Map<Integer, AttackType> CODE_TO_TYPE_MAP = new HashMap<>();
+
+    static {
+        for (AttackType type : values()) {
+            CODE_TO_TYPE_MAP.put(type.attackTypeCode, type);
+        }
+    }
 
     private final int attackTypeCode;
     private final String attackTypeName;
@@ -24,10 +33,11 @@ public enum AttackType {
     }
 
     public static AttackType fromCode(int attackTypeCode) {
-        return Stream.of(values())
-                .filter(attackType -> attackType.attackTypeCode == attackTypeCode)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(ATTACK_TYPE_EXCEPTION_MESSAGE));
+        return CODE_TO_TYPE_MAP.getOrDefault(attackTypeCode, throwException());
+    }
+
+    private static AttackType throwException() {
+        throw new IllegalArgumentException(ATTACK_TYPE_EXCEPTION_MESSAGE);
     }
 
     public int effectMp(int playerMp) {
