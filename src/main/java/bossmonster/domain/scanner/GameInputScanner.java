067c5c3780df91a59a046cdcb2550c9fe3fc558f
edgar6bf <edgar6bf@gmail.com>
2023-10-02 13:11:27 +0900

    feat: GameInputScanner 사용자 입력 기능 구현
    - 사용자로부터 정수 입력
    - 사용자로부터 문자열 입력
    - 사용자로부터 원하는 정수 개수 및 구분자를 기준으로 여러 정수 입력

---
+++ src/main/java/bossmonster/domain/scanner/GameInputScanner.java
@@ -0,0 +1,10 @@
+package bossmonster.domain.scanner;
+
+public interface GameInputScanner {
+
+    int inputNumber();
+
+    String inputString();
+
+    int[] inputStringAndConvertToIntegerArrayBasedOnDelimiter(int countNumber, String delimiter);
+}
