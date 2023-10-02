1bc20ce3a330dc021e04d117ba68a0c37480e86e
twkwon0417 <91003152+twkwon0417@users.noreply.github.com>
2023-10-02 12:39:37 +0900

    <feat> : player와 boss가 상호작용 할때의 method 구현

--- src/main/java/bossmonster/controller/GameController.java
+++ src/main/java/bossmonster/controller/GameController.java
@@ -23,4 +23,34 @@ public class GameController {
     private AttackType userInputAttackType() {
         return inputView.inputAttackType();
     }
+
+    private Boolean playerBossTransaction(Player player, BossMonster bossMonster, int count) {
+        AttackType playerAttackType = userInputAttackType();
+        try {
+            bossMonster.attacked(player.attack(playerAttackType));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return playerBossTransaction(player, bossMonster, count);
+        }
+
+        if (bossMonster.isDead()) {
+            OutputView.bossSad(bossMonster, player);
+            OutputView.playerWin(player, count);
+            return false;
+        }
+        
+        int bossAttackDamage = bossMonster.attack();
+        player.attacked(bossAttackDamage);
+        if (player.isDead()) {
+            OutputView.bossHappy(bossMonster, player);
+            OutputView.bossMonsterWin(player, count);
+            return false;
+        }
+
+        OutputView.bossSad(bossMonster, player);
+        OutputView.damageByPlayer(playerAttackType);
+        OutputView.damageByBossMonster(bossAttackDamage);
+
+        return playerBossTransaction(player, bossMonster, count + 1);
+    }
 }
