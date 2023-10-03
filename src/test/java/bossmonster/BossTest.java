87b4ace39a8208723a2efe89acbe3190f7d987dd
Taaate <an981022@naver.com>
2023-10-03 09:26:01 +0900

    test: 보스 관련 테스트 작성

---
+++ src/test/java/bossmonster/BossTest.java
@@ -0,0 +1,59 @@
+package bossmonster;
+
+import bossmonster.domain.Boss;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BossTest {
+
+    private static final String ERROR = "[ERROR] ";
+
+    @Test
+    void 정상_테스트() {
+        //given
+        String minBossHp = "100";
+        String maxBossHp = "300";
+
+        //when
+        Boss minBoss = new Boss(minBossHp);
+        Boss maxBoss = new Boss(maxBossHp);
+
+        //then
+        assertThat(minBoss.getHp()).isEqualTo(100);
+        assertThat(maxBoss.getHp()).isEqualTo(300);
+    }
+
+    @Test
+    void 문자_체력_예외() {
+        //given
+        String stringBossHp = "hp";
+
+        //when & then
+        assertThatThrownBy(() -> new Boss(stringBossHp))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR);
+    }
+
+    @Test
+    void 최소_체력_예외() {
+        //given
+        String lowBossHp = "99";
+
+        //when & then
+        assertThatThrownBy(() -> new Boss(lowBossHp))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR);
+    }
+
+    @Test
+    void 최대_체력_예외() {
+        //given
+        String highBossHp = "301";
+
+        //when & then
+        assertThatThrownBy(() -> new Boss(highBossHp))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR);
+    }
+}
