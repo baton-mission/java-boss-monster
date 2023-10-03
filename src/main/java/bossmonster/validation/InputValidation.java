678d7d3f635f8cedbd1c1b7eb0b6395019d4746e
ParkDH0809 <znxn1237@gmail.com>
2023-10-03 23:06:26 +0900

    feat(InpuValidation): 입력 오류 시 [ERROR]문구 추가

--- src/main/java/bossmonster/validation/InputValidation.java
+++ src/main/java/bossmonster/validation/InputValidation.java
@@ -2,6 +2,7 @@ package bossmonster.validation;
 
 import java.util.*;
 public class InputValidation {
+    private static final String ERROR_MESSAGE = "[ERROR] ";
     public static int validBossHP(String input) {
         int inputNumber = isNumber(input);
         isBetween100And300(inputNumber);
@@ -10,7 +11,7 @@ public class InputValidation {
 
     public static String validPlayerName(String input) {
         if(input.length() > 5) {
-            throw new IllegalArgumentException("5자 이하의 이름을 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + "5자 이하의 이름을 입력해주세요.");
         }
         return input;
     }
@@ -24,7 +25,7 @@ public class InputValidation {
 
     public static void isAdd300(List<Integer> playerHPMP) {
         if(playerHPMP.get(0) + playerHPMP.get(1) != 200) {
-            throw new IllegalArgumentException("HP와 MP의 합이 200이 되도록 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + "HP와 MP의 합이 200이 되도록 입력해주세요.");
         }
     }
 
@@ -34,14 +35,14 @@ public class InputValidation {
         try {
             stringArr = input.split(",");
         } catch (ArrayIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("숫자,숫자 형태로 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + "숫자,숫자 형태로 입력해주세요.");
         }
         return stringArr;
     }
 
     public static void isBetween100And300(int input) {
         if(input < 100 || 300 < input) {
-            throw new IllegalArgumentException("100 ~ 300 사이의 숫자를 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + "100 ~ 300 사이의 숫자를 입력해주세요.");
         }
     }
 
@@ -50,7 +51,7 @@ public class InputValidation {
         try {
             inputNumber = Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자를 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + "숫자를 입력해주세요.");
         }
         return inputNumber;
     }
