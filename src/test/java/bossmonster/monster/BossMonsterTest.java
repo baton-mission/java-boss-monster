26726c0090dc25037ef05aa0dbc447a80f4f224f
Hanjaemo <hjm2530@gmail.com>
2023-10-03 11:23:25 +0900

    test(BossMonsterTest): BossMonster 클래스 단위 테스트

--- src/test/java/bossmonster/monster/BossMonsterTest.java
+++ src/test/java/bossmonster/monster/BossMonsterTest.java
@@ -2,37 +2,67 @@ package bossmonster.monster;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import bossmonster.domain.characterattributes.Hp;
 import bossmonster.domain.characterattributes.Mp;
 import bossmonster.domain.characterattributes.Name;
 import bossmonster.domain.characterattributes.Stat;
 import bossmonster.domain.monster.BossMonster;
+import bossmonster.domain.monster.BossMonsterAppearance;
 import bossmonster.domain.player.Player;
+import bossmonster.service.Service;
 
 class BossMonsterTest {
 
-    @DisplayName("보스 몬스터 생성 시 HP가 최소 값보다 작은 경우 예외를 발생시킨다.")
-    @Test
-    void constructBossMonster_Fail_ByHpLessThanMinimum() {
-        // given
-        Hp hp = new Hp(80);
+    Service service;
 
+    @BeforeEach
+    void setUp() {
+        service = new Service();
+    }
+
+    @DisplayName("보스 몬스터를 생성 시 외형은 기본값을 가진다.")
+    @Test
+    void isAppearanceDefault_Success_ByGenerateBossMonster() {
+        // when
+        BossMonster bossMonster = service.generateBossMonster(100);
+
+        // then
+        assertThat(bossMonster.appearance()).isEqualTo(BossMonsterAppearance.DEFAULT.getAppearance());
+    }
+
+    @DisplayName("보스 몬스터 생성 시 HP는 입력한 HP와 같다.")
+    @ParameterizedTest
+    @ValueSource(ints = {100, 200})
+    void isHpSameWithInputHp_Success_ByGenerateBossMonster(int inputHp) {
+        // when
+        BossMonster bossMonster = service.generateBossMonster(inputHp);
+        int bossMonsterHp = bossMonster.hpAndInitialHp().get(0);
+
+        // then
+        assertThat(bossMonsterHp).isEqualTo(inputHp);
+    }
+
+    @DisplayName("보스 몬스터 생성 시 HP가 지정된 최소 값보다 작은 경우 실패한다.")
+    @Test
+    void generateBossMonster_Fail_ByHpLessThanMinimum() {
         // when, then
-        assertThatThrownBy(() -> new BossMonster(hp))
+        assertThatThrownBy(() -> service.generateBossMonster(40))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("보스 몬스터 생성 시 HP가 최대 값보다 큰 경우 예외를 발생시킨다.")
+    @DisplayName("보스 몬스터 생성 시 HP가 지정된 최소 값보다 작은 경우 실패한다.")
     @Test
-    void constructBossMonster_Fail_ByHpGreaterThanMaximum() {
-        // given
-        Hp hp = new Hp(350);
-
+    void generateBossMonster_Fail_ByHpGreaterThanMaximum() {
         // when, then
-        assertThatThrownBy(() -> new BossMonster(hp))
+        assertThatThrownBy(() -> service.generateBossMonster(400))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -40,17 +70,13 @@ class BossMonsterTest {
     @Test
     void reducePlayerHp_Success_ByAttack() {
         // given
-        Name name = new Name("jam");
-        Hp playerHp = new Hp(100);
-        Mp playerMp = new Mp(100);
-        Player player = new Player(name, new Stat(playerHp, playerMp));
-
-        BossMonster bossMonster = new BossMonster(new Hp(100));
+        Player player = service.generatePlayer("user", List.of(100, 100));
+        BossMonster bossMonster = service.generateBossMonster(100);
 
         // when
         bossMonster.attack(15, player);
 
         // then
-        assertThat(playerHp.getHp()).isEqualTo(85);
+        assertThat(player.stat().get(0)).isEqualTo(85);
     }
 }
