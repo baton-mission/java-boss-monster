1e6d9a3e6544b9df04c1c1e1cd65c3459f660ab3
ParkDH0809 <znxn1237@gmail.com>
2023-10-03 22:04:12 +0900

    feat(InputValidation): Player name, HP, MP 입력값 유효성 검사 구현

--- src/main/java/bossmonster/validation/InputValidation.java
+++ src/main/java/bossmonster/validation/InputValidation.java
@@ -6,6 +6,36 @@ class InputValidation {
         isBetween100And300(input);
     }
 
+    public static void validPlayerName(String input) {
+        if(input.length() > 5) {
+            throw new IllegalArgumentException("5자 이하의 이름을 입력해주세요.");
+        }
+    }
+
+    public static void validPlayerHPAndMP(String input) {
+        String[] stringArr = divideString(input);
+        isNumber(stringArr[0]);
+        isNumber(stringArr[1]);
+        isAdd300(stringArr);
+    }
+
+    public static void isAdd300(String[] stringArr) {
+        if(Integer.parseInt(stringArr[0]) + Integer.parseInt(stringArr[1]) != 200) {
+            throw new IllegalArgumentException("HP와 MP의 합이 200이 되도록 입력해주세요.");
+        }
+    }
+
+
+    public static String[] divideString(String input) {
+        String[] stringArr;
+        try {
+            stringArr = input.split(",");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("숫자,숫자 형태로 입력해주세요.");
+        }
+        return stringArr;
+    }
+
     public static void isBetween100And300(String input) {
         int inputInteger = Integer.parseInt(input);
         if(inputInteger < 100 || 300 < inputInteger) {
