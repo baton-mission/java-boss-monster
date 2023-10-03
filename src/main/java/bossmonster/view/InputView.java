fb0f8653c1968a322f1a0ac2a67002a99a718889
ParkDH0809 <znxn1237@gmail.com>
2023-10-03 23:41:48 +0900

    feat(InputView): 공격 방법 입력 구현

--- src/main/java/bossmonster/view/InputView.java
+++ src/main/java/bossmonster/view/InputView.java
@@ -8,6 +8,8 @@ public class InputView {
     private static final String INPUT_BOSS_HP = "보스 몬스터의 HP를 입력해주세요.";
     private static final String INPUT_PLAYER_NAME = "플레이어의 이름을 입력해주세요.";
     private static final String INPUT_PLAYER_HP_AND_MP = "플레이어의 HP와 MP를 입력해주세요.(,로 구분)";
+    private static final String INPUT_ATTACK_METHOD = "어떤 공격을 하시겠습니까?\n 1. 물리 공격\n 2. 마법 공격";
+
 
     static Scanner scanner = new Scanner(System.in);
 
@@ -24,5 +26,10 @@ public class InputView {
     public List<Integer> inputPlayerHpAndMp() {
         System.out.println(INPUT_PLAYER_HP_AND_MP);
         return InputValidation.validPlayerHPAndMP(scanner.nextLine());
+    }
+
+    public String inputAttackMethod() {
+        System.out.println(INPUT_ATTACK_METHOD);
+        return scanner.nextLine();
     }
 }
