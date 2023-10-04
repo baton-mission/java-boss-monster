b887e45f9f00b455dbd166dcc7b6115ec29ebd57
robinjoon <robin980108@naver.com>
2023-10-04 23:39:49 +0900

    fix(gameEngine) : 게임 엔진 인터페이스 수정.
    게임엔진이 출력기로 출력을 위임할 수 있도록 의존성 추가.

--- src/main/java/bossmonster/domain/GameEngine.java
+++ src/main/java/bossmonster/domain/GameEngine.java
@@ -1,11 +1,12 @@
 package bossmonster.domain;
 
 import bossmonster.view.InputProcessor;
+import bossmonster.view.OutputProcessor;
 
 public interface GameEngine {
     void initGame(InputProcessor inputProcessor);
     void startGame();
     void playerTurn();
     void bossTurn();
-    void endGame();
+    void endGame(OutputProcessor outputProcessor);
 }
