0448f3f8d0058c060d65a52fc4c96f2db2377503
wan0417 <kwontony123@gmail.com>
2023-10-06 01:13:10 +0900

    BossMonster 주석 삭제 및 final 추가

--- src/main/java/bossmonster/domain/BossMonster.java
+++ src/main/java/bossmonster/domain/BossMonster.java
@@ -3,9 +3,9 @@ package bossmonster.domain;
 import java.util.Random;
 
 public class BossMonster {
-    private static Random rand = new Random();
+    private static final Random rand = new Random();
 
-    private int maxHp;
+    private final int maxHp;
     private int currentHp;
 
     public BossMonster(int hp) {
@@ -21,14 +21,6 @@ public class BossMonster {
         return this.maxHp;
     }
 
-//    public static int validateBossHp(int hp) {
-//        if (hp > MAX_HP_SIZE || hp < MIN_HP_SIZE) {
-//            throw new IllegalArgumentException(
-//                    "[Error] Boss monster's health should be in between 100 to 300");
-//        }
-//        return hp;
-//    }
-
     public int attack() {
         return rand.nextInt(19) + 1;
     }
@@ -38,8 +30,6 @@ public class BossMonster {
     }
 
     public boolean isDead() {
-        if (currentHp <= 0)
-            return true;
-        return false;
+        return currentHp <= 0;
     }
 }
