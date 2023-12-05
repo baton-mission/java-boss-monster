d80550060a667ebc5e657941e1b7e8217b44cd96
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-06 00:37:16 +0900

    refactor(GameHistory): 공격 방식을 문자열 형태로만 저장하도록 변경

--- src/main/java/bossmonster/domain/GameHistory.java
+++ src/main/java/bossmonster/domain/GameHistory.java
@@ -1,5 +1,6 @@
 package bossmonster.domain;
 
+import bossmonster.domain.dto.GameHistoryDto;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +10,7 @@ public class GameHistory {
 
         private int turnCount;
         private String playerName;
-        private AttackType playerAttackType;
+        private String playerAttackType;
         private int playerAttackDamage;
         private int monsterAttackDamage;
         private int playerMaxHP;
@@ -20,7 +21,7 @@ public class GameHistory {
         private int monsterCurrentHP;
         private boolean gameStatus;
 
-        public Turns(int turnCount, AttackType playerAttackType, int playerAttackDamage,
+        public Turns(int turnCount, String playerAttackType, int playerAttackDamage,
                      int monsterAttackDamage, boolean gameStatus, Player player, BossMonster bossMonster) {
             this.turnCount = turnCount;
             this.playerName = player.getName();
@@ -37,19 +38,33 @@ public class GameHistory {
         }
     }
 
+    private static final String DEFAULT_ATTACK_TYPE = "";
     private static final int DEFAULT_DAMAGE = 0;
-    private List<Turns> turnHistory = new ArrayList<>();
+    private int recentRecord;
+    private List<Turns> raidHistory = new ArrayList<>();
 
     public void addHistory(int turnCount, boolean gameStatus, Player player, BossMonster bossMonster) {
-        turnHistory.add(
-                new Turns(turnCount, AttackType.ATTACK_TYPE_NONE, DEFAULT_DAMAGE, DEFAULT_DAMAGE, gameStatus, player,
+        raidHistory.add(
+                new Turns(turnCount, DEFAULT_ATTACK_TYPE, DEFAULT_DAMAGE, DEFAULT_DAMAGE, gameStatus, player,
                         bossMonster));
     }
 
-    public void addHistory(int turnCount, AttackType playerAttackType, int playerAttackDamage,
+    public void addHistory(int turnCount, String playerAttackType, int playerAttackDamage,
                            int monsterAttackDamage, boolean gameStatus, Player player, BossMonster bossMonster) {
-        turnHistory.add(
+        raidHistory.add(
                 new Turns(turnCount, playerAttackType, playerAttackDamage, monsterAttackDamage, gameStatus, player,
                         bossMonster));
+    }
+
+    public GameHistoryDto requestRaidHistory() {
+        setRecentRecord();
+        Turns turns = raidHistory.get(recentRecord);
+        return new GameHistoryDto(turns.turnCount, turns.playerName, turns.playerAttackType, turns.playerAttackDamage,
+                turns.monsterAttackDamage, turns.playerMaxHP, turns.playerCurrentHP, turns.playerMaxMP,
+                turns.playerCurrentMP, turns.monsterMaxHP, turns.monsterCurrentHP, turns.gameStatus);
+    }
+
+    private void setRecentRecord() {
+        recentRecord = raidHistory.size() - 1;
     }
 }
