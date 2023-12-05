40245b9ba0d4768924c3862e242c31e74c6a1a6e
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-05 22:33:57 +0900

    feat(AttackType):  공격 방식 관리 모델 구현

---
+++ src/main/java/bossmonster/domain/AttackType.java
@@ -0,0 +1,37 @@
+package bossmonster.domain;
+
+public enum AttackType {
+    ATTACK_TYPE_NONE(0, "없음", 0, 0, 0),
+    ATTACK_TYPE_NORMAL(1, "물리 공격", 10, 0, 10),
+    ATTACK_TYPE_MAGIC(2, "마법 공격", 20, 30, 0);
+
+    private final int number;
+    private final String name;
+    private final int damage;
+    private final int mpUsage;
+    private final int mpRecover;
+
+    AttackType(final int number, final String name, final int damage, final int mpUsage, final int mpRecover) {
+        this.number = number;
+        this.name = name;
+        this.damage = damage;
+        this.mpUsage = mpUsage;
+        this.mpRecover = mpRecover;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getMpUsage() {
+        return mpUsage;
+    }
+
+    public int getMpRecover() {
+        return mpRecover;
+    }
+}
