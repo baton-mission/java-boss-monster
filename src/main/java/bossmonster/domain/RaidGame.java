4760ce11988a09df0cf5835febeb889abaa65ded
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-05 21:58:35 +0900

    feat(RaidGame): 도메인 기능 수정 업데이트

--- src/main/java/bossmonster/domain/RaidGame.java
+++ src/main/java/bossmonster/domain/RaidGame.java
@@ -1,35 +1,68 @@
 package bossmonster.domain;
 
-import bossmonster.domain.dto.BossMonsterDto;
-import bossmonster.domain.dto.PlayerDto;
+import java.util.Arrays;
 import java.util.List;
 
 public class RaidGame {
 
     private BossMonster bossMonster;
     private Player player;
-    private int turns = 0;
+    private GameHistory gameHistory;
+    private boolean status;
+    private int turns;
+
+    public RaidGame() {
+        this.gameHistory = new GameHistory();
+        this.status = true;
+        this.turns = 0;
+    }
 
     public void createBossMonster(int hp) {
         bossMonster = new BossMonster(hp);
     }
 
-    public void createPlayer(String name, int hp, int mp) {
-        player = new Player(name, hp, mp);
+    public void createPlayer(String name, int maxHP, int maxMP) {
+        List<AttackType> attackType = Arrays.asList(AttackType.ATTACK_TYPE_NORMAL, AttackType.ATTACK_TYPE_MAGIC);
+        player = new Player(name, maxHP, maxMP, attackType);
     }
 
-    public BossMonsterDto requestBossMonsterStatus() {
-        return new BossMonsterDto(bossMonster.getTotalHP(), bossMonster.getCurrentHP());
+    public void executeTurn(AttackType attackType) {
+        if (isGameProgress()) {
+            increaseTurn();
+            int playerAttackDamage = executePlayerTurn(attackType);
+            int monsterAttackDamage = executeBossMonsterTurn();
+            createTurnHistory(attackType, playerAttackDamage, monsterAttackDamage);
+        }
     }
 
-    public PlayerDto requestPlayerStatus() {
-        return new PlayerDto(player.getName(), player.getTotalHP(), player.getCurrentHP(),
-                player.getTotalMP(), player.getCurrentMP());
+    private boolean isGameProgress() {
+        return status;
     }
 
-    public List<String> requestAttackType() {
-        return player.getAttackType();
+    private void setStatus(boolean status) {
+        this.status = status;
     }
 
-    public 
+    private void increaseTurn() {
+        turns ++;
+    }
+
+    private int executePlayerTurn(AttackType attackType) {
+        int playerAttackDamage = player.attack(attackType);
+        bossMonster.takeDamage(playerAttackDamage);
+        setStatus(bossMonster.isAlive());
+        return playerAttackDamage;
+    }
+
+    private int executeBossMonsterTurn() {
+        int monsterAttackDamage = bossMonster.attack();
+        player.takeDamage(monsterAttackDamage);
+        setStatus(player.isAlive());
+        return monsterAttackDamage;
+    }
+
+    private void createTurnHistory(AttackType playerAttackType, int playerAttackDamage, int monsterAttackDamage) {
+        gameHistory.addHistory(turns, playerAttackType, playerAttackDamage, monsterAttackDamage, status, player,
+                bossMonster);
+    }
 }
