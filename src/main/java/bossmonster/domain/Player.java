165fd73f85e651d3aedb062406feb4e7ecee0638
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-05 22:31:13 +0900

    feat(Player): 도메인 기능 수정 업데이트

--- src/main/java/bossmonster/domain/Player.java
+++ src/main/java/bossmonster/domain/Player.java
@@ -1,50 +1,60 @@
 package bossmonster.domain;
 
+import bossmonster.exception.ManaShortageException;
 import bossmonster.exception.Validator;
-import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class Player {
 
-    private static final String ATTACK_TYPE_NORMAL = "물리 공격";
-    private static final String ATTACK_TYPE_MAGIC = "마법 공격";
-
     private String name;
-    private int totalHP;
-    private int currentHP;
-    private int totalMP;
-    private int currentMP;
+    private PlayerStatus status;
+    private List<AttackType> attackType;
 
-    public Player(String name, int hp, int mp) {
+    public Player(String name, int maxHP, int maxMP, List<AttackType> attackType) {
         this.name = Validator.validatePlayerName(name);
-        Validator.validatePlayerStatus(hp, mp);
-        totalHP = hp;
-        totalMP = mp;
+        Validator.validatePlayerStatus(maxHP, maxMP);
+        this.status = new PlayerStatus(maxHP, maxMP);
+        this.attackType = attackType;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getTotalHP() {
-        return totalHP;
+    public boolean isAlive() {
+        return status.isAlive();
+    }
+
+    public int getMaxHP() {
+        return status.getMaxHP();
     }
 
     public int getCurrentHP() {
-        return currentHP;
+        return status.getCurrentHP();
     }
 
-    public int getTotalMP() {
-        return totalHP;
+    public int getMaxMP() {
+        return status.getMaxMP();
     }
 
     public int getCurrentMP() {
-        return currentMP;
+        return status.getCurrentMP();
     }
 
-    public List<String> getAttackType() {
-        return Arrays.asList(ATTACK_TYPE_NORMAL, ATTACK_TYPE_MAGIC);
+    public int attack(AttackType attackType) {
+        if (status.getCurrentMP() < attackType.getMpUsage()) {
+            throw new ManaShortageException();
+        }
+        recoverMP(attackType.getMpRecover());
+        return attackType.getDamage();
     }
 
+    public void takeDamage(int damage) {
+        status.setCurrentHP(damage);
+    }
 
+    private void recoverMP(int mp) {
+        status.setCurrentMP(mp);
+    }
 }
