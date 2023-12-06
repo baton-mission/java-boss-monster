a1465662112c24f7dc8d0d990d0d5cb996aff675
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-06 18:51:48 +0900

    feat(Message): 에러 메시지 추가

--- src/main/java/bossmonster/view/constants/Message.java
+++ src/main/java/bossmonster/view/constants/Message.java
@@ -33,8 +33,20 @@ public enum Message {
     OUTPUT_TURN_RESULT_PLAYER("\n" + "%s을 했습니다. (입힌 데미지: %d)"),
     OUTPUT_TURN_RESULT_BOSS("보스가 공격 했습니다. (입힌 데미지: %d)"),
     OUTPUT_GAME_OVER_PLAYER_WIN("\n" + "%s 님이 %d번의 전투 끝에 보스 몬스터를 잡았습니다."),
-    OUTPUT_GAME_OVER_PLAYER_LOSE("\n" + "%s의 HP가 %d이 되었습니다." + "\n" + "보스 레이드에 실패했습니다.");
+    OUTPUT_GAME_OVER_PLAYER_LOSE("\n" + "%s의 HP가 %d이 되었습니다." + "\n" + "보스 레이드에 실패했습니다."),
 
+    /* Error Message */
+    ERROR_EMPTY("입력값이 없습니다. 다시 입력해 주세요."),
+    ERROR_NOT_INTEGER("숫자만 입력 가능합니다. 다시 입력해 주세요."),
+    ERROR_NOT_COMMA("콤마(',')를 통해 구분해서 다시 입력해 주세요."),
+    ERROR_ARRAY_SIZE("입력된 항목이 많습니다. 다시 입력해 주세요."),
+    ERROR_BOSS_MONSTER_HP("보스몬스터의 HP는 100 이상 300 이하만  설정 가능합니다. 다시 입력해 주세요."),
+    ERROR_PLAYER_NAME("플레이어의 이름은 5자 이하만 가능합니다. 다시 입력해 주세요."),
+    ERROR_PLAYER_STATUS("플레이어의 HP와 MP의 합은 200으로만 설정 가능합니다. 다시 입력해 주세요."),
+    ERROR_PLAYER_ATTACK_TYPE("존재하지 않는 공격 방식입니다. 다시 입력해 주세요."),
+    ERROR_PLAYER_MANA_SHORTAGE("마나가 부족합니다. 다시 입력해 주세요.");
+
+    public static final String ERROR_PREFIX = "[ERROR] ";
     private final String message;
 
     Message(String message) {
@@ -43,5 +55,9 @@ public enum Message {
 
     public String getMessage() {
         return message;
+    }
+
+    public String getErrorMessage() {
+        return ERROR_PREFIX + message;
     }
 }
