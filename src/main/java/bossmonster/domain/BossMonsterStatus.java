75c08eaaff72d4a795201f53461d26dfb863cd5b
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-06 18:56:26 +0900

    refactor(BossMonsterStatus): 메서드 위치 변경

--- src/main/java/bossmonster/domain/BossMonsterStatus.java
+++ src/main/java/bossmonster/domain/BossMonsterStatus.java
@@ -24,15 +24,15 @@ public class BossMonsterStatus {
         return currentHP;
     }
 
+    private void setAlive(boolean status) {
+        alive = status;
+    }
+
     public void setCurrentHP(int currentHP) {
         this.currentHP = Math.min(maxHP, currentHP);
         if (currentHP < 0) {
             this.currentHP = 0;
         }
         setAlive(currentHP > 0);
-    }
-
-    private void setAlive(boolean status) {
-        alive = status;
     }
 }
