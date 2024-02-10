ec9de044c646735830e1402a93cce593d5f8f0df
Seonho Kim <5061ksh@naver.com>
2024-02-11 00:29:01 +0900

    refactor: change main method to start the program by executing method of gameUI

--- src/main/java/bossmonster/Main.java
+++ src/main/java/bossmonster/Main.java
@@ -3,7 +3,7 @@ package bossmonster;
 import bossmonster.controller.GameUI;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         GameUI gameUI = new GameUI();
         gameUI.execute();
     }
