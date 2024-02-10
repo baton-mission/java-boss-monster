83ee29bb2ade4c8c9483d494053df4ddee7d386f
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-02-10 16:07:28 +0900

    feat: set BossMonster

---
+++ src/main/java/bossmonster/BossMonster.java
@@ -0,0 +1,13 @@
+package bossmonster;
+
+public class BossMonster {
+    private final int hp;
+
+    public BossMonster(int hp){
+        this.hp = hp;
+    }
+
+    public int getBossMonsterHp(){
+        return this.hp;
+    }
+}
