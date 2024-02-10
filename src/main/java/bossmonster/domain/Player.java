9a0a26d90acccf98295e714aeb4f3440f3939c62
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-02-10 16:24:15 +0900

    feat: set Player

---
+++ src/main/java/bossmonster/domain/Player.java
@@ -0,0 +1,25 @@
+package bossmonster.domain;
+
+public class Player {
+    private final String name;
+    private final int hp;
+    private final int mp;
+
+    public Player(String name, int hp, int mp){
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getHp(){
+        return this.hp;
+    }
+
+    public int getMp(){
+        return this.mp;
+    }
+}
