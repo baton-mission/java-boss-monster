40389511e80cea08d7d19aa4590bc36988d900a4
Seonho Kim <5061ksh@naver.com>
2024-02-11 00:26:10 +0900

    refactor: fix printStat, printFigure method to change console gui

--- src/main/java/bossmonster/domain/Boss.java
+++ src/main/java/bossmonster/domain/Boss.java
@@ -3,21 +3,26 @@ package bossmonster.domain;
 import java.util.Random;
 
 public class Boss {
-    private Integer maxHp;
+    private final Integer maxHp;
     private Integer currentHp;
 
     public Boss(Integer hp) {
         this.maxHp = hp;
         this.currentHp = hp;
-        printFigure(true);
+        printStat();
+        printFigure(1);
     }
 
-    public void printFigure(final Boolean isInitial) {
-        System.out.println("============================");
-        System.out.println("BOSS HP [" + this.currentHp + "/" + this.maxHp + "]");
-        System.out.println("____________________________");
+    public Integer getHp() { return this.currentHp; }
 
-        if(isInitial) {
+    public void printStat() {
+        System.out.println("\n======================================");
+        System.out.println("BOSS HP [" + this.currentHp + "/" + this.maxHp + "]");
+        System.out.println("______________________________________");
+    }
+
+    public void printFigure(final Integer figureType) {
+        if(figureType == 1) {     //initial
             System.out.println(
                     "   ^-^\n"
                     + " / 0 0 \\\n"
@@ -25,21 +30,31 @@ public class Boss {
                     + " \\  -  /\n"
                     + "  - ^ -  "
             );
-            System.out.println("____________________________");
+            System.out.println("______________________________________");
+            return;
+        }
+        if(figureType == 0) {     //damaged
+            System.out.println(
+                    "   ^-^\n"
+                    + " / X X \\\n"
+                    + "(   \"\\  )\n"
+                    + " \\  -  /\n"
+                    + "  - ^ -  "
+            );
+            System.out.println("______________________________________");
             return;
         }
 
-        System.out.println(
+        System.out.println(      //lose
                 "   ^-^\n"
-                        + " / X X \\\n"
-                        + "(   \"\\  )\n"
-                        + " \\  -  /\n"
-                        + "  - ^ -  "
+                + " / ^ ^ \\\n"
+                + "(   \"\\  )\n"
+                + " \\  3  /\n"
+                + "  - ^ -  "
         );
-        System.out.println("____________________________");
+        System.out.println("______________________________________");
     }
 
-    public Integer getHp() { return this.currentHp; }
     public void decreaseHp(final Integer hp) {
         if(this.currentHp - hp < 0) {
             this.currentHp = 0;
