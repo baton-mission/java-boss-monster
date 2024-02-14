60f2362e5db2a934a0495c50e8cc3cd3e9562741
Choi-JY <Choi-JY1107@github.com>
2024-02-14 20:55:10 +0900

    feat: RaidController 생성

--- src/main/java/bossmonster/Main.java
+++ src/main/java/bossmonster/Main.java
@@ -1,7 +1,10 @@
 package bossmonster;
 
+import bossmonster.controller.RaidController;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+	public static void main(String[] args) {
+		RaidController raidController = new RaidController();
+		raidController.playGame();
+	}
 }
---
+++ src/main/java/bossmonster/controller/RaidController.java
@@ -0,0 +1,15 @@
+package bossmonster.controller;
+
+import bossmonster.view.InputView;
+import bossmonster.view.OutputView;
+
+public class RaidController {
+	private static final InputView inputView = new InputView();
+	private static final OutputView outputView = new OutputView();
+
+	public RaidController() {
+	}
+
+	public void playGame() {
+	}
+}
