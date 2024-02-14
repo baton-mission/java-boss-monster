35b9590204cd6099f32a65a174fb92c19d9605ac
Choi-JY <Choi-JY1107@github.com>
2024-02-15 05:06:27 +0900

    feat: Monster, Player을 DTO로 변환하는 기능 추가

--- src/main/java/bossmonster/service/RaidService.java
+++ src/main/java/bossmonster/service/RaidService.java
@@ -2,12 +2,23 @@ package bossmonster.service;
 
 import bossmonster.domain.Monster;
 import bossmonster.domain.Player;
+import bossmonster.dto.MonsterDTO;
+import bossmonster.dto.PlayerDTO;
 
 public class RaidService {
 	private static Monster monster;
 	private static Player player;
 
 	public RaidService() {
+	}
+
+	public static MonsterDTO getMonsterDTO() {
+		return new MonsterDTO(monster.getNowHp(), monster.getMaxHp());
+	}
+
+	public static PlayerDTO getPlayerDTO() {
+		return new PlayerDTO(player.getName(), player.getNowHp(), player.getNowMp(),
+			player.getMaxHp(), player.getMaxMp());
 	}
 
 	public void createMonster(int monsterHp) {
