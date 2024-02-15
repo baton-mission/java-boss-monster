890007af2d318657e26a112f88fdb740aa35adbf
Choi-JY <Choi-JY1107@github.com>
2024-02-15 14:00:27 +0900

    feat: 공격 관련 메소드 생성

--- src/main/java/bossmonster/constant/PlayerConstant.java
+++ src/main/java/bossmonster/constant/PlayerConstant.java
@@ -4,8 +4,10 @@ public enum PlayerConstant {
 	SUM_OF_HP_MP(200),
 	COUNT_OF_HP_MP(2),
 	LENGTH_OF_NAME(5),
-	PHYSICAL(1),
-	MAGIC(2);
+	PHYSICAL_ATTACK(1),
+	PHYSICAL_DAMAGE(10),
+	MAGIC_ATTACK(2),
+	MAGIC_DAMAGE(20);
 
 	private final int constant;
 
--- src/main/java/bossmonster/service/RaidService.java
+++ src/main/java/bossmonster/service/RaidService.java
@@ -1,5 +1,9 @@
 package bossmonster.service;
 
+import static bossmonster.constant.PlayerConstant.*;
+import static bossmonster.view.message.ErrorMessage.*;
+
+import bossmonster.constant.PlayerConstant;
 import bossmonster.domain.Monster;
 import bossmonster.domain.Player;
 import bossmonster.dto.MonsterDTO;
@@ -27,5 +31,35 @@ public class RaidService {
 
 	public void createPlayer(String playerName, int playerHp, int playerMp) {
 		player = new Player(playerName, playerHp, playerMp);
+	}
+
+	private int attackMonsterByPhysical() {
+		int damage = PHYSICAL_DAMAGE.getConstant();
+		return damage;
+	}
+
+	private int attackMonsterByMagic() {
+		int damage = MAGIC_DAMAGE.getConstant();
+		return damage;
+	}
+
+	public int attackByPlayer(PlayerConstant playerConstant) {
+		if (playerConstant == PHYSICAL_ATTACK)
+			return attackMonsterByPhysical();
+		if (playerConstant == MAGIC_ATTACK)
+			return attackMonsterByMagic();
+
+		throw new IllegalArgumentException(PLAYER_SHOULD_ATTACK_MONSTER.getMessage());
+	}
+
+	public int attackByMonster() {
+		int damage = 0;
+		return damage;
+	}
+
+	public boolean isEndGame() {
+		if (monster.getNowHp() <= 0 || player.getNowHp() <= 0)
+			return true;
+		return false;
 	}
 }
