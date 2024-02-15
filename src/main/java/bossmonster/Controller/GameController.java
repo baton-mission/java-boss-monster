2bb31e2e4c42619acbf45c5f93f4a665d9d4aa81
polarpolar914 <kdw2003914@naver.com>
2024-02-15 19:11:14 +0900

    feat: print Start message

--- src/main/java/bossmonster/Controller/GameController.java
+++ src/main/java/bossmonster/Controller/GameController.java
@@ -10,8 +10,15 @@ public class GameController {
     }
 
     public int getAttackType() {
+        System.out.println("어떤 공격을 하시겠습니까?\n"
+                + "1. 물리 공격\n"
+                + "2. 마법 공격");
         int attackType = scanner.nextInt();
 
         return attackType;
+    }
+
+    public void printStartMessage() {
+        System.out.println("보스 레이드를 시작합니다!\n");
     }
 }
